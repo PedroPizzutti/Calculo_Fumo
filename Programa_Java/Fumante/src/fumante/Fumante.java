20a3667ffa07b75e854c50619e9332ff7fb2222d
Pedro Pizzutti <pedropizzutti@gmail.com>
2021-08-26 20:43:58 -0300

    Inserindo o Programa

---
+++ Programa_Java/Fumante/src/fumante/Fumante.java
@@ -0,0 +1,19 @@
+package fumante;
+
+/**
+ *
+ * @author pedro
+ */
+public class Fumante {
+    
+    private CalculoFumante cfFumante;
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        CalculoFumante cfFumante = new CalculoFumante();
+        
+        cfFumante.setVisible(true);
+    }
+    
+}
